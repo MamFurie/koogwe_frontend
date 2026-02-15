1c4f3766fb7e50ed9596c3502cb371fc58db1277
MamFurie <mambizahkpemoua@gmail.com>
2026-02-15 19:30:40 +0000

    Fix: mise a jour

--- lib/ride_preview_screen.dart
+++ lib/ride_preview_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:koogwz_mobile/Searching_driver_screen.dart';
+import 'package:koogwz_mobile/searching_driver_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'ride_tracking_screen.dart';
 import 'searching_driver_screen.dart'; // ✅ AJOUTÉ
